c96e31a4337f174c4b3167dd0b8fe541c3099a22
nirvanush <nirvanushergo@gmail.com>
2022-06-04 23:41:39 +0300

    added TransactionJson

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import Transaction from './Transaction';
+import Transaction, { TransactionJson } from './Transaction';
 import Box, { SigmaType } from './Box';
 import { NANO_ERG_IN_ERG } from './constants';
 
 export default Transaction;
-export { Box, SigmaType, NANO_ERG_IN_ERG };
+export { Box, SigmaType, NANO_ERG_IN_ERG, TransactionJson };

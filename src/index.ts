1ea815e6ee52a9fda763422203a9b445d3f4d485
nirvanush <nirvanushergo@gmail.com>
2022-07-23 16:18:23 +0300

    expose ergo explorer api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Transaction, { TransactionJson } from './Transaction';
 import Box, { SigmaType, ExplorerBox } from './Box';
 import { NANO_ERG_IN_ERG } from './constants';
+import { explorerService } from './wallet/explorer/explorerService';
 
 export default Transaction;
 export { Box, SigmaType, NANO_ERG_IN_ERG, TransactionJson, ExplorerBox };
+export { explorerService };
